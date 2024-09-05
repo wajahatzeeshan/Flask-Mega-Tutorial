8402e9b24c1116c6f81854838b9807c9e5307556
wzeeshan <wajahatzeeshan@gmail.com>
2024-09-05 16:30:46 +0100

    conf

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 import os
 
-
 ENVIRONMENT = 'development'
 FLASK_APP = 'my-app'
 FLASK_DEBUG = True
 SECRET_KEY = 'GDtfD^&$%@^8tgYjD'
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get(SECRET_KEY) or 'you-will-never-guess'
