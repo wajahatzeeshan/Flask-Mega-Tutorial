246f14061f9b44a794edf57aa6d21cb913d31756
wzeeshan <wajahatzeeshan@gmail.com>
2024-09-05 16:24:56 +0100

    added forms

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,10 @@
-from flask import Flask
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'Wajahat Zeeshan'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Karachi!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movies are so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+# ...
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
